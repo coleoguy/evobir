fbe235fda8daede45c03aac05c97750593b82392
coleoguy <hlb7922@biol109034.local>
2013-11-11 09:51:38 -0500

    benchmarking tests

--- inst/wf.model/server.R
+++ inst/wf.model/server.R
@@ -30,9 +30,11 @@ ShinyPopGen <- function(fitness, initial.A, pop, gen, var.plot, iter){
       babies <-  c(rep(1, each = round(pop*A^2)), 
                    rep(2, each = round(pop*2*A*(1-A))), 
                    rep(3, each = round(pop*(1-A)^2)))
-      pop.fit <- vector()                       # fitness for each offspring
-      for(j in 1:length(babies)){               # this loop goes through the population
-        if(babies[j] == 1){
+      if(heath=='preset') pop.fit <- vector(length = pop)                       # fitness for each offspring
+      if(heath=='fly') pop.fit <- vector() 
+      pop.fit <- vector(length = )                       # fitness for each offspring
+      for(j in 1:length(babies)){               # THIS IS CRAPPY CODE
+        if(babies[j] == 1){                     # this loop goes through the population
           pop.fit[j] <- fitness[1]
         }else if(babies[j] == 2){
           pop.fit[j] <- fitness[2]
