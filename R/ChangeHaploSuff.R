fff28d3e991e7bad8ef935b17f5a57fb98ad30e0
coleoguy <hlb7922@biol109034.local>
2013-11-14 12:47:11 -0500

    extracted genome sim files

--- R/ChangeHaploSuff.R
+++
@@ -1,9 +0,0 @@
-ChangeHaploSuff <- function(x, y, haplosuff.mut, loci.s) {   # Mutations that extend the SLR(sex limited region)   # x = population    y = result of function RadDose
-  newhap.val <- runif(1, min = 0, max = haplosuff.mut)
-  foobish <- x[[y[[2]]]]  #this pulls out the genome we are mutating
-  which.locus <- sample(1:loci.s, 1)
-  which.hap <- sample(c(13, 14), 1)
-  foobish[which.hap, which.locus] <- newhap.val
-  foobish -> x[[y[[2]]]]
-  return(x)
-}
--- R/Crossover.R
+++
@@ -1,8 +0,0 @@
-Crossover <- function(x, dist, loci, crossovers){                      # given current chrom (0, 2) , dist between rows of matrix and length of chrom calls crossovers with mean equal to spec.
-  if(x == 0){                                               # I need this if so it can have a high probability of staying in current state...lame
-    foo <- sample(c(0, dist), 1, c(loci / crossovers, 1), replace = T)  # this is the opportunity to cross over
-  }else{
-    foo <- sample(c(dist, 0), 1, c(loci/crossovers, 1), replace = T)  # this is the opportunity to cross over
-  }
-  return(foo)
-} 
