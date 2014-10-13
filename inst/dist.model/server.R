4c308de94c660514c146e2b381c951750cfa7013
coleoguy <hlb7922@biol109034.local>
2014-10-13 13:00:33 -0500

    test

--- inst/dist.model/server.R
+++ inst/dist.model/server.R
@@ -1,9 +1,19 @@
 library(shiny)
 shinyServer(function(input, output) {
   x <- reactive({
-    seq(input$N.mean - 4 * input$N.sd, 
-        input$N.mean + 4 * input$N.sd, 
-        length = 200)
+    if(select == 1){
+      seq(input$N.mean - 4 * input$N.sd, 
+          input$N.mean + 4 * input$N.sd, 
+          length = 200)
+    }
+    if(select == 2){
+      seq(input$N.mean - 6 * input$N.sd, 
+          input$N.mean + 6 * input$N.sd, 
+          length = 200)
+    }
+    
+    
+    
   })
   y <- reactive({
     dnorm(x(), mean = input$N.mean, sd=input$N.sd)
