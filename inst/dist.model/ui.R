4482885a30ca8b8af13bf27aef78f4b7b060f816
coleoguy <hlb7922@biol109034.local>
2014-10-13 11:46:02 -0500

    test

--- inst/dist.model/ui.R
+++ inst/dist.model/ui.R
@@ -5,14 +5,12 @@ shinyUI(pageWithSidebar(
     selectInput("select", label = "Select Statistical Distribution",
                 choices = list("Normal" = 1, "Exponential" = 2,
                                "Gamma" = 3, "Logistic" = 4), selected = 1), 
-    if(select == 1){
     tags$div(class="header", checked=NA,
              tags$p("Normal Distribution")),
     sliderInput("N.mean", "mean:", 
                 min = -100, max = 100, value=0, step = 5),
     sliderInput("N.sd", "standard deviation:", 
                 min = 0, max = 10, value=1, step = .25),
-    }
 
     sliderInput("time", "Time for simulation:", 
                 min = 4, max = 20, value=10, step = 1),
