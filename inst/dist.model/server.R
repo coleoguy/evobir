7765b54f549333b1b21f786cd17bb813c56fe2ed
coleoguy <hlb7922@biol109034.local>
2014-10-13 12:58:03 -0500

    test

--- inst/dist.model/server.R
+++ inst/dist.model/server.R
@@ -1,13 +1,15 @@
 library(shiny)
 shinyServer(function(input, output) {
-  tree <- reactive({
-    x <- seq(input$N.mean - 4 * input$N.sd, 
-             input$N.mean + 4 * input$N.sd, 
-             length = 200)
-    y <- dnorm(x, mean = input$N.mean, sd=input$N.sd)
+  x <- reactive({
+    seq(input$N.mean - 4 * input$N.sd, 
+        input$N.mean + 4 * input$N.sd, 
+        length = 200)
+  })
+  y <- reactive({
+    dnorm(x, mean = input$N.mean, sd=input$N.sd)
   })
   output$treePlot <- renderPlot({
-      plot(x=tree()$x, y=tree()$y, col = "red", type="l", lwd=3)
+      plot(x=x(), y=y(), col = "red", type="l", lwd=3)
   })  
 })
 
