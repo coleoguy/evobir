2e8339a6140d7326e9a137222b88cc4bebdf4441
coleoguy <hlb7922@biol109034.local>
2014-10-13 11:32:12 -0500

    test

--- inst/dist.model/server.R
+++ inst/dist.model/server.R
@@ -8,7 +8,7 @@ shinyServer(function(input, output) {
       
  # })
   output$treePlot <- renderPlot({
-    plot(1:10,1:10)
+    plot(input$N.mean:10,input$sd:10)
 #    if(select == 1){
 #      tree(N.mean, N.sd)
 #    }
