3de07703af9890a0dc9f5ba9b1f1adde9bbf6f08
coleoguy <hlb7922@biol109034.local>
2014-10-13 11:27:38 -0500

    test

--- inst/dist.model/server.R
+++ inst/dist.model/server.R
@@ -1,12 +1,12 @@
 library(shiny)
 shinyServer(function(input, output) {
-  tree <- reactive({
+ # tree <- reactive({
     #if(select == 1){
     #  x <- seq(N.mean - 4 * N.sd, N.mean + 4 * N.sd, length = 200)
     #  y <- dnorm(x, mean = N.mean, N.sd)
     #}
       
-  })
+ # })
   output$treePlot <- renderPlot({
     plot(1:10,1:10)
 #    if(select == 1){
