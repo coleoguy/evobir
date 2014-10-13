638fdc61f18775bdbd12f28e137b082e662995dc
coleoguy <hlb7922@biol109034.local>
2014-10-13 13:01:05 -0500

    test

--- inst/dist.model/server.R
+++ inst/dist.model/server.R
@@ -1,12 +1,12 @@
 library(shiny)
 shinyServer(function(input, output) {
   x <- reactive({
-    if(select == 1){
+    if(input$select == 1){
       seq(input$N.mean - 4 * input$N.sd, 
           input$N.mean + 4 * input$N.sd, 
           length = 200)
     }
-    if(select == 2){
+    if(input$select == 2){
       seq(input$N.mean - 6 * input$N.sd, 
           input$N.mean + 6 * input$N.sd, 
           length = 200)
