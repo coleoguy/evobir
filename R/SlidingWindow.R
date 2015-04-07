a0b39351a51e821406e1c49fe16055782421fd79
Heath Blackmon <coleoguy@gmail.com>
2015-04-07 11:17:10 -0500

    sliding window
    changed sliding mean to a general function allowing any function to be
    applied within windows

--- R/SlidingWindow.R
+++ R/SlidingWindow.R
@@ -1,9 +1,9 @@
-SlidingMean <- function(data, window, step){
+SlidingWindow <- function(FUN, data, window, step){
   total <- length(data)
   spots <- seq(from = 1, to = (total - window), by = step)
   result <- vector(length = length(spots))
   for(i in 1:length(spots)){
-    result[i] <- mean(data[spots[i]:(spots[i] + window - 1)])
+    result[i] <- match.fun(FUN)(data[spots[i]:(spots[i] + window - 1)])
   }
   return(result)
 }
