a8a058d6d4ff1be3d5e3bd6ff2153d99bd7f00cd
Heath Blackmon <coleoguy@gmail.com>
2015-04-17 10:56:12 -0500

    added concatenate function

---
+++ R/SuperMatrix.R
@@ -0,0 +1,52 @@
+superMatrix <- function(missing = "-", format = "f",
+                        file = "conc.alignment.fasta"){
+  # get file names
+  file.names <- list.files()
+  # read DNA
+  DNA <- list()
+  for(i in 1:length(file.names)){
+    DNA[[i]] <- read.dna(file=file.names[i], 
+                         format = format, 
+                         as.character=T)
+  }
+  # calculate total alignment lenght
+  total.length <- 0
+  for(i in 1:length(DNA)){
+    total.length <- total.length + ncol(DNA[[i]])
+  }
+  # get all taxa names
+  taxa <- vector()
+  for(i in 1:length(DNA)){
+    counter <- length(taxa) + 1
+    for(j in 1:nrow(DNA[[i]])){
+      taxa <- c(taxa, 
+                row.names(DNA[[i]])[!row.names(DNA[[i]]) %in% taxa])
+    }
+  }
+  # create empty alignment
+  seqmatrix <- matrix(as.character(missing),
+                                    length(taxa),
+                                    total.length)
+  rownames(seqmatrix) <- taxa
+  # create partition record holder
+  partitions <- as.data.frame(matrix(,length(DNA),3))
+  colnames(partitions) <- c("part", "start", "stop")
+  #build the supermatrix
+  c.col <- 0
+  for(i in 1:length(DNA)){
+    gene <- DNA[[i]]
+    for(j in 1:nrow(gene)){
+      print(j)
+      c.row <- which(rownames(seqmatrix) == rownames(gene)[j])
+      seqmatrix[c.row, (c.col + 1):(c.col + ncol(gene))] <- gene[j, ]
+    }
+    partitions[i,1:3] <- c(file.names[i], c.col + 1, c.col + ncol(gene))
+    c.col <- c.col + ncol(gene)
+  }
+  results <- list()
+  results[[1]] <- partitions
+  results[[2]] <- seqmatrix
+  write.dna(seqmatrix, file=file, format="f")
+  write.csv(partitions, row.names=F, file="partitions.csv")
+  return(results)
+}
