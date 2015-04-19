c4c8c9fd9f2d022ac5eee45961d9521bd0306cd6
Heath Blackmon <coleoguy@gmail.com>
2015-04-19 13:14:24 -0500

    added a error message
    non numeric inputs will now throw an intelligible error message

--- R/Even.R
+++ R/Even.R
@@ -1,6 +1,9 @@
 Even <- function(x){
   result <- vector()
-  #you should test whether its numeric and return an intelligable error
+  if(!is.numeric(x)){
+    cat("Even only works on numbers")
+    stop()
+  }
   for(i in 1:length(x)){
     if(x[i]/2 == round(x[i]/2)){
       result[i] <- T
