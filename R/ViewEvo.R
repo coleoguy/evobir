e988267f0446a4a0410d07711d9cb1b8e8edf651
Heath Blackmon <coleoguy@gmail.com>
2015-09-25 16:43:01 -0500

    updates to include the new brownian motion simulator

--- R/ViewEvo.R
+++ R/ViewEvo.R
@@ -2,4 +2,5 @@ ViewEvo <- function(simulation){
   if(simulation == 'wf.model') shiny::runApp(system.file("wf.model", package='evobiR'))
   if(simulation == 'bd.model') shiny::runApp(system.file("bd.model", package='evobiR'))
   if(simulation == 'dist.model') shiny::runApp(system.file("dist.model", package='evobiR'))
+  if(simulation == 'bm.model') shiny::runApp(system.file("bm.model", package='evobiR'))
 }
---
+++ inst/bm.model/server.R
@@ -0,0 +1,33 @@
+library(shiny)
+# I like this color pallete much more than the base options
+library(viridis)
+shinyServer(function(input, output) {
+  # this is the actual brownian motion simulation
+  x <- reactive({
+    # insure reproducability
+    set.seed <- input$seed.val
+    replicate(input$reps, cumsum(rnorm(input$gens)))
+  })
+  # Now we will build the output plot
+  output$distPlot <- renderPlot({  
+    # we will have two plots
+    par(mfcol=c(2,1))
+    # need a bit more room 
+    par(mar=c(3,1,1,1))
+    # this just keeps our color variation from being hidden when itterations++
+    cols <- sample(viridis(input$reps))
+    # the primary plot
+    matplot(x(),type="l",lty=1, lwd=1.5, col=cols, main="Brownian Motion", ylab="Trait Value", xlab="Generations")
+    # this line will indicate what is being plotted in the histogram below
+    abline(v=input$mon, lwd=3, col="red")
+    # now we plot the distribution switched from a histrogram this looks prettier IMHO
+    plot(density(x()[input$mon,]), 
+         xlim=c(min(x()),max(x())),
+         col = 'red', 
+         xlab="Trait value",
+         ylab="",
+         lwd=3,
+         main=paste("Trait distribution at generation", input$mon))
+    
+  })
+})
