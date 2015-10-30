63a33d0f5fced5e06b27b24f08e5adfc9fec9b99
Heath Blackmon <coleoguy@gmail.com>
2015-10-30 16:58:04 -0500

    add ShowTree function
    just a wrapper for plot and tiplabel from APE

---
+++ R/ShowTree.R
@@ -0,0 +1,6 @@
+ShowTree <- function(tree, tip.vals, pch = 16, cols = NULL, tip.cex=1){
+  if(is.null(cols)) cols <- viridis(length(unique(tip.vals)))
+  plot(tree, show.tip.label = F)
+  # make a color vector for the tips
+  tiplabels(pch=pch,col=cols[factor(tip.vals)], frame="none", cex=tip.cex)
+}
