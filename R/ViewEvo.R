3feb6e06568092384f3469ecf31a021333eb7454
Heath Blackmon <coleoguy@gmail.com>
2015-11-03 17:58:25 -0600

    updates to namespace
    changing from imports to importFrom

--- R/ViewEvo.R
+++ R/ViewEvo.R
@@ -1,8 +1,8 @@
 ViewEvo <- function(simulation){
-  if(simulation == 'wf.model') shiny::runApp(system.file("wf.model", package='evobiR'))
-  if(simulation == 'bd.model') shiny::runApp(system.file("bd.model", package='evobiR'))
-  if(simulation == 'dist.model') shiny::runApp(system.file("dist.model", package='evobiR'))
-  if(simulation == 'bm.model') shiny::runApp(system.file("bm.model", package='evobiR'))
-  if(simulation == 'clumping.model') shiny::runApp(system.file("clumping.model", package='evobiR'))
-  if(simulation == 'bm.tree.model') shiny::runApp(system.file("bm.tree.model", package='evobiR'))
+  if(simulation == 'wf.model') runApp(system.file("wf.model", package='evobiR'))
+  if(simulation == 'bd.model') runApp(system.file("bd.model", package='evobiR'))
+  if(simulation == 'dist.model') runApp(system.file("dist.model", package='evobiR'))
+  if(simulation == 'bm.model') runApp(system.file("bm.model", package='evobiR'))
+  if(simulation == 'clumping.model') runApp(system.file("clumping.model", package='evobiR'))
+  if(simulation == 'bm.tree.model') runApp(system.file("bm.tree.model", package='evobiR'))
 }
