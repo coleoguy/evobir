7b0e0d2508d51293af5c1cc27fef76cc815dc789
Heath Blackmon <coleoguy@gmail.com>
2017-03-15 12:55:35 -0500

    fix

--- R/normalize.R
+++ R/normalize.R
@@ -10,9 +10,9 @@ Normalize <- function(x, MARGIN=1){
     y <- lapply(x, vec.norm)
   }
   if(is.matrix(x)){    
-    # 1 = rows    
-    # 2 = columns
-    # 3 = whole matrix
+    #  1 = rows    
+    #  2 = columns
+    #  3 = whole matrix
     if(MARGIN == 1 | MARGIN == 2) y <- apply(x, MARGIN = MARGIN, FUN = vec.norm)
     if(MARGIN == 3) y <- matrix(vec.norm(unlist(x)), nrow(x), ncol(x))
   }
