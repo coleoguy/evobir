f44f52c866ce3d00b90839c03a60a106e9ccabf5
Heath Blackmon <coleoguy@gmail.com>
2017-05-17 15:53:51 -0500

    fix typos

--- R/Even.R
+++ R/Even.R
@@ -1,15 +1,7 @@
 Even <- function(x){
-  result <- vector()
   if(!is.numeric(x)){
     cat("Even only works on numbers")
     stop()
   }
-  for(i in 1:length(x)){
-    if(x[i]/2 == round(x[i]/2)){
-      result[i] <- T
-    }else{
-      result[i] <- F
-    }
-  }
-  return(result)
+  x %% 2 == 0
 }
