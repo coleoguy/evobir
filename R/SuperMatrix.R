97c8dcffb0d24787ad454ddcdaae9162f484415e
Heath Blackmon <coleoguy@gmail.com>
2018-06-13 14:12:54 -0500

    expand function for no concatenation

--- R/SuperMatrix.R
+++ R/SuperMatrix.R
@@ -2,7 +2,8 @@ SuperMatrix <- function(missing = "-",
                         prefix = "concatenated",
                         save = T,
                         format.in = "f",
-                        format.out = "f"){
+                        format.out = "f",
+                        concatenate = T){
   # get file names
   file.names <- list.files()
   # read DNA
@@ -13,11 +14,6 @@ SuperMatrix <- function(missing = "-",
                          format = format.in, 
                          as.character=T)
   }
-  # calculate total alignment length
-  total.length <- 0
-  for(i in 1:length(DNA)){
-    total.length <- total.length + ncol(DNA[[i]])
-  }
   # get all taxa names
   taxa <- vector()
   for(i in 1:length(DNA)){
@@ -27,39 +23,68 @@ SuperMatrix <- function(missing = "-",
                 row.names(DNA[[i]])[!row.names(DNA[[i]]) %in% taxa])
     }
   }
-  # create empty alignment
-  seqmatrix <- matrix(as.character(missing),
-                                    length(taxa),
-                                    total.length)
-  rownames(seqmatrix) <- taxa
-  # create partition record holder
-  partitions <- as.data.frame(matrix(,length(DNA),3))
-  colnames(partitions) <- c("part", "start", "stop")
-  #build the supermatrix
-  c.col <- 0
-  print("Creating supermatrix")
-  for(i in 1:length(DNA)){
-    print(paste("Processing alignment", i))
-    gene <- DNA[[i]]
-    print(i)
-    for(j in 1:nrow(gene)){
-      c.row <- which(rownames(seqmatrix) == rownames(gene)[j])
-      seqmatrix[c.row, (c.col + 1):(c.col + ncol(gene))] <- gene[j, ]
+  
+  if(concatenate == T){
+    # calculate total alignment length
+    total.length <- 0
+    for(i in 1:length(DNA)){
+      total.length <- total.length + ncol(DNA[[i]])
     }
-    partitions[i,1:3] <- c(file.names[i], c.col + 1, c.col + ncol(gene))
-    c.col <- c.col + ncol(gene)
+    # create empty alignment
+    seqmatrix <- matrix(as.character(missing),
+                        length(taxa),
+                        total.length)
+    rownames(seqmatrix) <- taxa
+    # create partition record holder
+    partitions <- as.data.frame(matrix(,length(DNA),3))
+    colnames(partitions) <- c("part", "start", "stop")
+    #build the supermatrix
+    c.col <- 0
+    print("Creating supermatrix")
+    for(i in 1:length(DNA)){
+      print(paste("Processing alignment", i))
+      gene <- DNA[[i]]
+      print(i)
+      for(j in 1:nrow(gene)){
+        c.row <- which(rownames(seqmatrix) == rownames(gene)[j])
+        seqmatrix[c.row, (c.col + 1):(c.col + ncol(gene))] <- gene[j, ]
+      }
+      partitions[i,1:3] <- c(file.names[i], c.col + 1, c.col + ncol(gene))
+      c.col <- c.col + ncol(gene)
+    }
+    results <- list()
+    results[[1]] <- partitions
+    results[[2]] <- seqmatrix
+    if(save == T){
+      print("saving files")
+      write.dna(seqmatrix, 
+                file = paste(prefix, ".fasta", sep = ""), 
+                format = format.out)
+      write.csv(partitions, 
+                row.names = F, 
+                file = paste(prefix, ".partitions.csv", sep = ""))
+    }
   }
-  results <- list()
-  results[[1]] <- partitions
-  results[[2]] <- seqmatrix
-  if(save == T){
-    print("saving files")
-    write.dna(seqmatrix, 
-              file = paste(prefix, ".fasta", sep = ""), 
-              format = format.out)
-    write.csv(partitions, 
-              row.names = F, 
-              file = paste(prefix, ".partitions.csv", sep = ""))
+
+  if(concatenate == F){
+    results <- list()
+    for(i in 1:length(DNA)){
+      seqmatrix <- matrix(as.character(missing),
+                          length(taxa),
+                          ncol(DNA[[i]]))
+      rownames(seqmatrix) <- taxa
+      for(j in 1:nrow(DNA[[i]])){
+        hit <- which(row.names(DNA[[i]])[j] == row.names(seqmatrix))
+        seqmatrix[hit, ] <- DNA[[i]][j, ]
+      }
+      results[[i]] <- seqmatrix
+      if(save == T){
+      write.dna(seqmatrix, 
+                file = paste(prefix, file.names[i], ".fasta", sep = ""), 
+                format = format.out)
+      }
+    }
+  # create empty alignment
   }
   return(results)
 }
