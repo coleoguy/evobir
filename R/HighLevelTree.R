72f54ea7a7d4affc4b3e8592dc36199175ea22ea
Heath Blackmon <coleoguy@gmail.com>
2018-06-23 13:05:45 -0500

    add HighLevelTree

---
+++ R/HighLevelTree.R
@@ -0,0 +1,11 @@
+HighLevelTree <- function(taxa.table, tree, cur.tips, new.tips){
+  ## get one species per family
+  new<-subset(taxa.table,!duplicated(taxa.table[,new.tips]))
+  ## drop all but one rep per clade
+  tree.pruned <- drop.tip(tree, tree$tip.label[!tree$tip.label %in% new[,cur.tips]])
+  ## lame but swaps species and family names
+  for(i in 1:length(tree.pruned$tip.label)){
+    tree.pruned$tip.label[i]<-as.character(new[new[,cur.tips] == tree.pruned$tip.label[i],new.tips])
+  }
+  return(tree.pruned)
+}
