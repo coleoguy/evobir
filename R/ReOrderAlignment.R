eec268208dbf6a9c8cdb9b6071c3b61028ae3840
Heath Blackmon <coleoguy@gmail.com>
2023-01-10 14:37:34 -0600

    reorder functions

--- R/ReOrderAlignment.R
+++ R/ReOrderAlignment.R
@@ -1,80 +1,34 @@
-# re-order alignment base on physical mapping 
-# Sean Chien 
-# count - until hit the first nucleotide
-# re-order them by number of '-' before the first nucleotide
-# reference sequences go to front. vector number or character?
-# in order to run this function, seqinr is required. 
-
-# check if references are in the file 
-# print references 
-# library(seqinr)
-######################################################################################
-# Intro
-# re-ordering sequences based on physical mapping in order to visualize easier.
-# file for original file's path and name. newfile for new file's path and name. 
-# ref is optional. It is a vector and can contain either numeric or character. 
-# ref default is off. If it is on, the reference sequence(s) will be in the beginning.
-######################################################################################
-
 ReOrderAlignment <- function(file, newfile, ref=NULL){
   dat <- read.fasta(file)
-  ### no reference
-  if (is.null(ref)){
-    result = c()
-    print("Re-ordering...")
-    for (j in 1:length(dat)){
-      dash = 0
-      for (i in 1:length(dat[[j]])){
-        if (dat[[j]][i] == "-"){
-          dash <- dash + 1
-        }
-        else if (!dat[[j]][i] == "-") break
-      }
-      result[j]<- dash
-    } 
-    newlist<-dat[order(result)]
-    write.fasta(newlist, names = names(newlist), file.out = newfile)
-    print("Done!")
+  # validate inputs
+  if(!is.null(ref)){
+    if(is.numeric(ref[1])){
+      ref <- names(dat)[ref]
+    }
+    if(!sum(ref %in% names(dat)) == length(ref)){
+      stop("One of your reference sequences was not found in your file")
+    }
   }
-  
-  ### with reference(s)
-  # find where is the references and take it away?
-  # check if the references are in the file first.
-  if (!is.null(ref)){
-    for (i in 1:length(ref)){
-      if ((names(dat[ref][i])) %in% names(dat)){
-        print(paste("The sequence you picked: ", names(dat[ref][i])))
-        if (i == length(ref)){
-          print("Re-ordering...")
-          # taking off the reference(s) for the original data and run re-order
-          norefname<-names(dat)[-c(match(names(dat[ref]),names(dat)))]
-          noreferdat<-dat[norefname]
-          # run 
-          result = c()
-          for (j in 1:length(noreferdat)){
-            dash = 0
-            for (i in 1:length(noreferdat[[j]])){
-              if (noreferdat[[j]][i] == "-"){
-                dash <- dash + 1
-              }
-              else if (!noreferdat[[j]][i] == "-") break
-            }
-            result[j]<- dash
-          } 
-          newlist<-noreferdat[order(result)]
-          newlist<-c(dat[c(match(names(dat[ref]),names(dat)))],newlist)
-          write.fasta(newlist, names = names(newlist), file.out = newfile)
-          print("Done!")
-        }
-      }
-      # if the references are not in the file
-      else if (!(names(dat[ref][i])) %in% names(dat)){
-        print(paste("Oops! the sequence,'",ref[i],"',is not in your file. Please check again."))
-        break
-        # stop the whole process
+  # find correct order
+  startspots <- c()
+  for(i in 1:length(dat)){
+    if(dat[[i]][1] != "-"){
+      startspots[i] <- 1
+    }else{
+      bar <- which(dat[[i]] == "-")
+      vec <- bar[-1] - bar[-length(bar)]
+      if(length(vec) == sum(vec)){
+        startspots[i] <- length(vec)
+      }else{
+        startspots[i] <- min(which(vec != 1))
       }
     }
   }
+  seqorder <- order(startspots)
+  starts <- which(names(dat) %in% ref)
+  seqorder <- c(starts, seqorder[!seqorder %in% starts])
+  # make new seq list
+  newlist <- dat[seqorder]
+  write.fasta(newlist, names = names(newlist), file.out = newfile)
+  print("Done!")
 }
-
-
